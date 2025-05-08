b53dbc2bee8e482b343d29a3814320fbb5bce372
Ashley Garriga <140339056+Garriga11@users.noreply.github.com>
2025-05-07 19:02:21 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,10 @@ const PedestrianDetector = dynamic(() => import('@/app/components/PedestrianDete
 export default function PedestrianPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-xl font-bold mb-4">Pedestrian Detection</h1>
+      <div className="text-justify mb-4 text-red-500">
+        Disclaimer: This is a demo application. 
+        The detection model is not trained for production use and may not perform accurately.To use the app the  click  yes for permission. Wait for the image to load, it could take several minutes.</div>
+      <h1 className="text-xl font-bold mb-4">Human Detection Model</h1>
       <div className="w-full max-w-[900px]">
         {/* Render the dynamically imported component */}
         <PedestrianDetector />
